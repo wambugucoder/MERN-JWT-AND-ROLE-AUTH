02028ed64b0426f6a6c0eeea784d68f72a6aaebe
adelpro <47066151+adelpro@users.noreply.github.com>
2023-02-14 16:41:05 +0100

    Update keys.js
    forgotten " typo

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: "YOUR MONGO URI",
     secretOrKey: "secret",
-    googleClientID: "YOUR CLIENT ID,
+    googleClientID: "YOUR CLIENT ID",
   googleClientSecret: "YOUR CLIENT SECRET",
   Cookie:"YOUR COOKIE SECRET"
   };
